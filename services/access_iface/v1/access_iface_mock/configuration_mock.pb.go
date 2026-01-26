2a258f3ac2e6def7f75a7f9efb0ee11fbbb13870
vaziria <manorder123@gmail.com>
2026-01-26 15:34:19 +0700

    add configuration proto

---
+++ services/access_iface/v1/access_iface_mock/configuration_mock.pb.go
@@ -0,0 +1,100 @@
+package access_iface_mock
+
+import (
+    "context"
+    "reflect"
+    gomock "github.com/golang/mock/gomock"
+    connect "connectrpc.com/connect"
+    v1 "github.com/pdcgo/schema/services/access_iface/v1"
+)
+
+type MockConfigurationService struct {
+    ctrl     *gomock.Controller
+    recorder *MockConfigurationServiceMockRecorder
+}
+
+type MockConfigurationServiceMockRecorder struct {
+    mock *MockConfigurationService
+}
+
+func NewMockConfigurationService(ctrl *gomock.Controller) *MockConfigurationService {
+    mock := &MockConfigurationService{ctrl: ctrl}
+    mock.recorder = &MockConfigurationServiceMockRecorder{mock}
+    return mock
+}
+
+func (m *MockConfigurationService) EXPECT() *MockConfigurationServiceMockRecorder {
+    return m.recorder
+}
+
+type MockConfigurationServiceClient struct {
+    ctrl     *gomock.Controller
+    recorder *MockConfigurationServiceClientMockRecorder
+}
+
+type MockConfigurationServiceClientMockRecorder struct {
+    mock *MockConfigurationServiceClient
+}
+
+func NewMockConfigurationServiceClient(ctrl *gomock.Controller) *MockConfigurationServiceClient {
+    mock := &MockConfigurationServiceClient{ctrl: ctrl}
+    mock.recorder = &MockConfigurationServiceClientMockRecorder{mock}
+    return mock
+}
+
+func (m *MockConfigurationServiceClient) EXPECT() *MockConfigurationServiceClientMockRecorder {
+    return m.recorder
+}
+
+func (m *MockConfigurationService) ExtensionConfiguration(ctx context.Context, req *connect.Request[v1.ExtensionConfigurationRequest]) (*connect.Response[v1.ExtensionConfigurationResponse], error) {
+    m.ctrl.T.Helper()
+    ret := m.ctrl.Call(m, "ExtensionConfiguration", ctx, req)
+    ret0, _ := ret[0].(*connect.Response[v1.ExtensionConfigurationResponse])
+    ret1, _ := ret[1].(error)
+    return ret0, ret1
+}
+
+func (mr *MockConfigurationServiceMockRecorder) ExtensionConfiguration(ctx, req interface{}) *gomock.Call {
+    mr.mock.ctrl.T.Helper()
+    return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ExtensionConfiguration", reflect.TypeOf((*MockConfigurationService)(nil).ExtensionConfiguration), ctx, req)
+}
+
+func (m *MockConfigurationServiceClient) ExtensionConfiguration(ctx context.Context, req *connect.Request[v1.ExtensionConfigurationRequest]) (*connect.Response[v1.ExtensionConfigurationResponse], error) {
+    m.ctrl.T.Helper()
+    ret := m.ctrl.Call(m, "ExtensionConfiguration", ctx, req)
+    ret0, _ := ret[0].(*connect.Response[v1.ExtensionConfigurationResponse])
+    ret1, _ := ret[1].(error)
+    return ret0, ret1
+}
+
+func (mr *MockConfigurationServiceClientMockRecorder) ExtensionConfiguration(ctx, req interface{}) *gomock.Call {
+    mr.mock.ctrl.T.Helper()
+    return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ExtensionConfiguration", reflect.TypeOf((*MockConfigurationService)(nil).ExtensionConfiguration), ctx, req)
+}
+
+func (m *MockConfigurationService) ExtensionConfigurationReplace(ctx context.Context, req *connect.Request[v1.ExtensionConfigurationReplaceRequest]) (*connect.Response[v1.ExtensionConfigurationReplaceResponse], error) {
+    m.ctrl.T.Helper()
+    ret := m.ctrl.Call(m, "ExtensionConfigurationReplace", ctx, req)
+    ret0, _ := ret[0].(*connect.Response[v1.ExtensionConfigurationReplaceResponse])
+    ret1, _ := ret[1].(error)
+    return ret0, ret1
+}
+
+func (mr *MockConfigurationServiceMockRecorder) ExtensionConfigurationReplace(ctx, req interface{}) *gomock.Call {
+    mr.mock.ctrl.T.Helper()
+    return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ExtensionConfigurationReplace", reflect.TypeOf((*MockConfigurationService)(nil).ExtensionConfigurationReplace), ctx, req)
+}
+
+func (m *MockConfigurationServiceClient) ExtensionConfigurationReplace(ctx context.Context, req *connect.Request[v1.ExtensionConfigurationReplaceRequest]) (*connect.Response[v1.ExtensionConfigurationReplaceResponse], error) {
+    m.ctrl.T.Helper()
+    ret := m.ctrl.Call(m, "ExtensionConfigurationReplace", ctx, req)
+    ret0, _ := ret[0].(*connect.Response[v1.ExtensionConfigurationReplaceResponse])
+    ret1, _ := ret[1].(error)
+    return ret0, ret1
+}
+
+func (mr *MockConfigurationServiceClientMockRecorder) ExtensionConfigurationReplace(ctx, req interface{}) *gomock.Call {
+    mr.mock.ctrl.T.Helper()
+    return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ExtensionConfigurationReplace", reflect.TypeOf((*MockConfigurationService)(nil).ExtensionConfigurationReplace), ctx, req)
+}
+
